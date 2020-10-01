e30553e396862e9e4ec8a7b428f4edbc5fcd8b58
Ewarbs <69345974+Ewarbs@users.noreply.github.com>
2020-10-01 11:41:14 +0100

    Added API to JS

--- src/app.js
+++ src/app.js
@@ -1,1 +1,4 @@
-alert("Hello World");
+let apiKey = "cbec302bbeb6e69212aeaf2b33e22b4c";
+letapiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid={apiKey`;
+
+console.log(apiUrl);

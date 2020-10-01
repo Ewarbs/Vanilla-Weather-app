392615fb1ee931fcd290fcee4c76489a3e8228d4
Ewarbs <69345974+Ewarbs@users.noreply.github.com>
2020-10-01 14:01:16 +0100

    Removed Console log

--- src/app.js
+++ src/app.js
@@ -22,7 +22,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
 
